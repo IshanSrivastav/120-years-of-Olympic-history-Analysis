dd7707f37e06c72014c357e25c015d6439320457
IshanSrivastav <ishankumarsrivastav0001@gmail.com>
2024-10-01 15:25:09 +0530

    Change Title

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ region_df = pd.read_csv('noc_regions.csv')
 
 df = preprocessor.preprocess(df, region_df)
 
-st.sidebar.title("Olympics Analysis")
+st.sidebar.title("Olympics Analysis 1896-2016")
 st.sidebar.image(
    'https://e7.pngegg.com/pngimages/1020/402/png-clipart-2024-summer-olympics-brand-circle-area-olympic-rings-olympics-logo-text-sport.png')
 user_menu = st.sidebar.radio(
